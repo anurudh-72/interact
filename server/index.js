82e5c37dff9b1546b4a0b554bc317a9c088b5a35
anurudh-72 <97913332+anurudh-72@users.noreply.github.com>
2022-07-04 12:51:06 +0530

    save

--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ const CONNECTION_URL = 'mongodb+srv://project1:project1@atlascluster.hyros.mongo
 
 app.get('/', (req, res) => {
     res.send('Welcome');
-});
+})
 
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL)
-.then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
+mongoose.connect(CONNECTION_URL, {})
+.then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT))) 
 .catch((error) => console.log(error.message));
 
 // mongoose.connect(CONNECTION_URL).then(()=>{console.log('Runin on : ' + PORT)});
